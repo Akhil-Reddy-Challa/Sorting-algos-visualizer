523023332d55cc2ca442093b3b223ef9331e07b8
Akhil Reddy <akhilreddychalla98@gmail.com>
2020-06-24 10:50:44 +0530

    Stable

--- src/visualizerSource/Visualizer.jsx
+++ src/visualizerSource/Visualizer.jsx
@@ -1,32 +1,32 @@
 import React, { Component } from "react";
 
-const getColor = {
-  "background-color": "#1affff",
-};
 class Visualizer extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
       list: [],
-      NUMBER_OF_ARRAY_BARS: 4,
+      NUMBER_OF_ARRAY_BARS: 3,
     };
   }
   getNumberOnBar = (bar_count) => {
     if (bar_count <= 40) return "aliceblue";
     else return "transparent";
   };
-  getWidthOfBars = (bar_count) => {
-    let width; //set default value
-    if (bar_count <= 7) width = 265;
-    else if (bar_count <= 20) width = 91.5;
-    else if (bar_count <= 40) width = 44;
-    else if (bar_count <= 80) width = 20;
-    else if (bar_count <= 160) width = 8;
-    else if (bar_count <= 213) width = 4;
-    else width = 3;
+  getWidthOfBars = (bar_count, screen_width) => {
+    //Now we have screen_width to dyanmically adjust with various devices
+    //1) We have bar_width + margin(default: 2px) in CSS
+    var width;
+    //Now decrease screen_width by 20px (Because of the Scroll bar intervention)
+    screen_width -= 20;
+    //Now decrease the margin size from screen_width, because each bar consumes 2px has margin-right
+    //Decrease total screen_width by (margin * Number_of_bars)
+    screen_width = screen_width - bar_count * 2;
+
+    width = screen_width / bar_count; //Now this gives us the width for number_of_bars
+    console.log(width);
+
     return width;
-    //console.log(width);
   };
   generateValuesInArray = (NUMBER_OF_ARRAY_BARS) => {
     const min = 1;
@@ -38,48 +38,40 @@ class Visualizer extends Component {
   };
   generateNewValues = () => {
     //Now delete all elements in array, so that we can use above method(generateValuesInArray)
-    this.state.list.length = 0; //This will empty the array( Reference: https://stackoverflow.com/questions/1232040/how-do-i-empty-an-array-in-javascript)
+    //this.state.list.length = 0; //This will empty the array( Reference: https://stackoverflow.com/questions/1232040/how-do-i-empty-an-array-in-javascript)
+    this.setState({ list: [] });
     //console.log(this.state.NUMBER_OF_ARRAY_BARS);
     //console.log("I am here now ");
     this.generateValuesInArray(this.state.NUMBER_OF_ARRAY_BARS);
   };
   reSizeArray = (event) => {
-    //console.log("User Selected: ", event.target.value);
+    console.log("User Selected: ", event.target.value);
     //User select a value
     //Now update the array size
-    this.state.NUMBER_OF_ARRAY_BARS = event.target.value;
-    this.generateNewValues();
+    this.setState({ NUMBER_OF_ARRAY_BARS: event.target.value }, () => {
+      this.generateNewValues();
+    });
+    /*
+    The setState() operation is asynchronous and hence your console.log() will be executed before the setState() mutates the values and hence you see the result.
+
+    To solve this: we have included this.geneateNewValues() in it's call back area
+    */
   };
   sortValues = () => {
     //Bubble Sort
     let array = this.state.list;
     let temp = 0;
-    let counter = 1;
     let leng_of_array = array.length;
     for (let i = 0; i < leng_of_array - 1; i++) {
       for (let j = 0; j < leng_of_array - 1; j++) {
-        console.log("Two elements(j,j+1) are: ", j, j + 1);
-        //Now highlight the color of the two elements
-        setTimeout(() => {
-          document.getElementById(j).setAttribute("class", "redBar");
-          document.getElementById(j + 1).setAttribute("class", "redBar");
-        }, counter * 1000);
         if (array[j] > array[j + 1]) {
-          console.log(
-            "Elements being swapped are==================================> ",
-            array[j],
-            array[j + 1]
-          );
           temp = array[j];
           array[j] = array[j + 1];
           array[j + 1] = temp;
         }
-        counter++;
       }
     }
-    //this.setState({ array });
-
-    clearTimeout();
+    this.setState({ array });
   };
 
   traverseArray = (bar_color) => {
@@ -111,7 +103,11 @@ class Visualizer extends Component {
   };
   render() {
     const { list } = this.state;
-    let widthOfBar = this.getWidthOfBars(this.state.NUMBER_OF_ARRAY_BARS);
+    let widthOfBar = this.getWidthOfBars(
+      this.state.NUMBER_OF_ARRAY_BARS,
+      window.innerWidth
+    );
+
     let displayNumberOnBar = this.getNumberOnBar(
       this.state.NUMBER_OF_ARRAY_BARS
     );
@@ -126,9 +122,9 @@ class Visualizer extends Component {
             type="range"
             id="slider"
             name="Slider"
-            defaultValue="7"
-            min="7"
-            max="273"
+            defaultValue="3"
+            min="3"
+            max="400"
             onChange={this.reSizeArray}
           />
           <nav>
@@ -155,6 +151,7 @@ class Visualizer extends Component {
             <div
               className="normalBar"
               id={index}
+              key={index}
               style={{ height: number * 3, width: widthOfBar }}
             >
               <p style={{ color: displayNumberOnBar }}>{number}</p>
